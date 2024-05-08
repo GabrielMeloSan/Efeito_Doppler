098a982bab4097db78fb99dfd42e3263123459c1
Gabriel Melo Santos <gabriel.melo.santos.15@gmail.com>
2024-05-08 17:03:06 -0300

    Adição da classe main para testes de funcionalidade

---
+++ PBL/main.java
@@ -0,0 +1,23 @@
+//arquivo de testes
+
+
+import java.util.Scanner;
+
+import PBL.SenoECosseno;
+
+public class main {
+    public static void main(String[] args) {
+
+        SenoECosseno calc = new SenoECosseno();
+
+        System.out.println("Inserir valor de seno e cosseno em rad: ");
+
+        Scanner scn = new Scanner(System.in);
+
+        double x = scn.nextDouble();
+
+        //precisao arbitraria de 0.0001
+        System.out.println("Seno: " + Double.toString(calc.CalculaSeno(x, 0.0001)));
+        System.out.println("Coseno: " + Double.toString(calc.CalculaCosseno(x, 0.0001)));
+    }
+}
