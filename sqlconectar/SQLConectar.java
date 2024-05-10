112722282ccb2f9ff31eae416dc1a4e783ac6121
Matheus Souza <132164629+matheuWR8@users.noreply.github.com>
2024-05-10 20:44:32 -0300

    Commit final do programa de teste.
    Implementação do resultSet e exibição da tabela

--- sqlconectar/SQLConectar.java
+++ sqlconectar/SQLConectar.java
@@ -21,10 +21,10 @@ public class SQLConectar {
     // Criação da tabela
     static void criaTabela(Connection conexao){
         String createTabela = "USE Banco " // comando da criação da tabela
-                    + "CREATE TABLE Pessoa("
-                    + "id int not null primary key,"
-                    + "nome varchar(40),"
-                    + "idade int) ";
+                            + "CREATE TABLE Pessoa("
+                            + "id int not null primary key,"
+                            + "nome varchar(40),"
+                            + "idade int) ";
         
         try(PreparedStatement stm = conexao.prepareStatement(createTabela)){
             stm.executeUpdate();
@@ -45,18 +45,22 @@ public class SQLConectar {
     }
     
     // Seleciona os dados da tabela Pessoa em um resultset
-    static ResultSet selectDados(Connection conexao) throws SQLException{
+    static void selectDados(Connection conexao) throws SQLException{
         ResultSet rst;
-        String selectTabela = "SELECT * FROM Pessoa"; // comando select
+        String selectTabela = "SELECT id,nome,idade FROM Pessoa"; // comando select
         
         try(PreparedStatement stm = conexao.prepareStatement(selectTabela)){
             rst = stm.executeQuery(); // execução e armazenamento dos resultados
+            while (rst.next()){       // o ResultSet só pode ser usado enquanto seu statement não for fechado 
+                System.out.print("ID: " + rst.getInt("id"));
+                System.out.print("| Nome: " + rst.getString("nome"));
+                System.out.print("| Idade: " + rst.getInt("idade") + "\n");
+            }
         }
-        return rst;
     }
     
 
-    public static void main(String[] args) throws SQLException{
+    public static void main(String[] args){
         
         Conexao conectar = new Conexao(); // instancia o objeto de conexão
         Connection conexao = conectar.getConnection(); // estabelece uma conexão com o servidor
@@ -64,27 +68,40 @@ public class SQLConectar {
         criaBanco(conexao); // cria o banco e a tabela caso não existam
         criaTabela(conexao);
         
-        System.out.println("Escolha uma opção: \n[1] Cadastrar pessoa; \n[2] Exibir cadastros");
-        String opcao = leitor.next();
-        if (opcao.equals("1")){
-            System.out.print("ID: ");
-            int id = leitor.nextInt();
-            leitor.nextLine();
-            System.out.print("Nome: ");
-            String nome = leitor.nextLine();
-            System.out.print("Idade: ");
-            int idade = leitor.nextInt();
-            
-            try{
-                inserirDados(conexao, id, nome, idade);
-            } catch (SQLException e){
-                System.out.println("ERRO: " + e.getMessage());
-            }
-        } else if(opcao.endsWith("2")){
-            // trabalharemos nisso
+        while(true){
+            System.out.println("Escolha uma opção: \n[1] Cadastrar pessoa; \n[2] Exibir cadastros; \n[0] Sair");
+            String opcao = leitor.next();
+            if (opcao.equals("1")){
+                System.out.print("ID: ");
+                int id = leitor.nextInt();
+                leitor.nextLine();
+                System.out.print("Nome: ");
+                String nome = leitor.nextLine();
+                System.out.print("Idade: ");
+                int idade = leitor.nextInt();
+
+                try{
+                    inserirDados(conexao, id, nome, idade);
+                } catch (SQLException e){
+                    System.out.println("ERRO: " + e.getMessage());
+                }
+            } else if(opcao.equals("2")){
+                try{
+                    selectDados(conexao);
+                } catch (SQLException e) {
+                    System.out.println("ERRO: " + e.getMessage());
+                }
+            } else if(opcao.equals("0")){
+                try{
+                    conexao.close();
+                    System.out.println("Conexão encerrada");
+                } catch (SQLException e){
+                    System.out.println("ERRO: " + e.getMessage());
+                }
+                break;
+            } else System.out.println("Digite uma opcao válida.");
         }
                 
-        conexao.close();
-        System.out.println("Conexão encerrada");
+        
     }
 }
