24de65539d845ea0daa9ba33764cc0745aae31e8
Matheus Souza <132164629+matheuWR8@users.noreply.github.com>
2024-05-25 19:31:18 -0300

    Correção de um parâmetro + trycatch ao invés de throw

--- PBL/src/MVC/Controller/InsercaodeDados.java
+++ PBL/src/MVC/Controller/InsercaodeDados.java
@@ -9,7 +9,7 @@ import src.MVC.DAO.Conexao;
 import src.MVC.DAO.SimulacaoDAO;
 
 public class InsercaodeDados {
-    public static void CalcularFisica(double frequencia_inicial, double distancia_inicial, double velocidade_relativa, double potencia, String nome_audio) throws SQLException, IOException{
+    public static void CalcularFisica(double frequencia_inicial, double distancia_inicial, double velocidade_relativa, double potencia, String nome_audio){
         
         //Insere os dados de física
         DadosFisica dados = new DadosFisica(frequencia_inicial, distancia_inicial, velocidade_relativa, potencia, nome_audio);
@@ -18,8 +18,14 @@ public class InsercaodeDados {
         GeraSom.CriaAudio(dados.CalculaFrequenciaAprox(velocidade_relativa, frequencia_inicial), dados.CalculaFrequenciaAfast(velocidade_relativa, frequencia_inicial), dados.getTempo(), dados.getNome_do_audio());
 
         //Parte de BD vai aqui abaixo:
-        SimulacaoDAO dao = new SimulacaoDAO(Conexao.getConnection());
-        dao.execProcedureInsertEmissor(dados.getFrequenciaInicial(), dados.getVelocidadeRelativa(), dados.getPotencia(), 340);
-        dao.execProcedureInsertSimulacoes(dados.getDistanciaInicial(), dados.getTempo(), dados.getIntensidade(), dados.getFreqPercebidaAfast(), dados.getFreqPercebidaAprox(), dados.getNome_do_audio());    
+        try{
+            SimulacaoDAO dao = new SimulacaoDAO(Conexao.getConnection());
+            dao.execProcedureInsertEmissor(dados.getFrequenciaInicial(), dados.getVelocidadeRelativa(), dados.getPotencia(), 340);
+            dao.execProcedureInsertSimulacoes(dados.getDistanciaInicial(), dados.getTempo(), dados.getIntensidade(), dados.getFreqPercebidaAfast(), dados.getFreqPercebidaAprox(), dados.getNome_do_audio()+".wav"); 
+        } catch (SQLException e){
+            System.out.println("ERRO: " + e.getMessage());
+        } catch (IOException e){
+            System.out.println("ERRO: " + e.getMessage());
+        }
     }
 }
